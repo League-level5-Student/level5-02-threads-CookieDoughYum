5573ff5db69af587696e1adc4d304914719cf0a0
root <root@16424db850fb>
2021-04-26 23:56:22 +0000

    Working on AdvancedRobotRace.

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -4,24 +4,74 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
-static Robot Matt=new Robot(700, 700);
-static Robot Joe=new Robot(700, 700);
-static Robot Sandy=new Robot(700, 700);
-static Robot Bob=new Robot(700, 700);
-static Robot Bill=new Robot(700, 700);
+ Robot Matt=new Robot(700, 700);
+ Robot Joe=new Robot(700, 700);
+ Robot Sandy=new Robot(700, 700);
+ Robot Bob=new Robot(700, 700);
+ Robot Bill=new Robot(700, 700);
 
 public static void main(String[] args) {
-	Matt.setSpeed(10);
-	Joe.setSpeed(10);
-	Sandy.setSpeed(10);
-	Bob.setSpeed(10);
-	Bill.setSpeed(10);
-	
-	Matt.moveTo(200, 250);
-	Joe.moveTo(400, 250);
-	Sandy.moveTo(600, 250);
-	Bob.moveTo(300, 350);
-	Bill.moveTo(500, 350);
+	OlympicRings_Threaded or=new OlympicRings_Threaded();
+	or.Robots();
 }
+void Robots() {
+	Thread r1=new Thread(()-> {
+	Matt.setSpeed(5);
+    Matt.moveTo(200, 250);
+	Matt.penDown();
+	for(int i=0; i<360; i++) {
+		Matt.turn(1);
+		Matt.move(2);
+	}
+	});
+	
+	Thread r2=new Thread(()-> {
+		Joe.setSpeed(5);
+		Joe.moveTo(400, 250);
+		Joe.penDown();
+		for(int i=0; i<360; i++) {
+			Joe.turn(1);
+			Joe.move(2);
+		}
+		});
+	
+	Thread r3=new Thread(()-> {
+		Sandy.setSpeed(5);
+		Sandy.moveTo(600, 250);
+		Sandy.penDown();
+		for(int i=0; i<360; i++) {
+			Sandy.turn(1);
+			Sandy.move(2);
+		}
+		});
+	
+	Thread r4=new Thread(()-> {
+		Bob.setSpeed(5);
+		Bob.moveTo(300, 350);
+		Bob.penDown();
+		for(int i=0; i<360; i++) {
+			Bob.turn(1);
+			Bob.move(2);
+		}
+		});
+	
+	Thread r5=new Thread(()-> {
+		Bill.setSpeed(5);
+		Bill.moveTo(500, 350);
+		Bill.penDown();
+		for(int i=0; i<360; i++) {
+			Bill.turn(1);
+			Bill.move(2);
+		}
+		});
+	
+	r1.start();
+	r2.start();
+	r3.start();
+	r4.start();
+	r5.start();
+	
+}
+
 }
 
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,27 @@
 package _02_Advanced_Robot_Race;
 
+import org.jointheleague.graphical.robot.Robot;
 public class AdvancedRobotRace {
-	// Re-do the robot race recipe from level 3 module 0. 
+	Robot a;
+	Robot b;
+	Robot c;
+	Robot d;
+	Robot e;
+	 // Re-do the robot race recipe from level 3 module 0. 
 	// This time, use threads to make all of the robots go at the same time.
-}
+	static Robot[] robots= new Robot[5];
+	public static void main(String[] args) {
+		for(int i=0; i<5; i++) {
+			robots[i]=new Robot();
+		}
+		AdvancedRobotRace arr=new AdvancedRobotRace();
+		arr.race();
+	}
+	void race() {
+		Thread r1=new Thread(()-> {
+			
+		});
+		
+		}
+	}
+
